af7db0905eefacedc83e6e6a4444f97099e2286a
Weiran Wang <weiran.wang@weiranwang-ltm.internal.salesforce.com>
2020-06-16 12:24:55 -0700

    adding recon

--- ddca/solver.py
+++ ddca/solver.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence
 from torch.nn.utils.rnn import pad_packed_sequence
+import math
 import logging
 
 class KERNEL(nn.Module):
