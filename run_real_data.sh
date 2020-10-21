0d8c4a20e80d3b5424840cab68dbd806c680851a
Gilbertheywood <bjw_sjtu@sjtu.edu.cn>
2020-10-21 18:36:47 -0400

    clean up the repo

--- run_real_data.sh
+++
@@ -1,1 +0,0 @@
-python real_data.py --lr 1e-3 --encoder_type gru --dropout 0.7 --rate_lambda 1.0 --ortho_lambda 0.1 --recon_lambda 500 --epochs 25 --masked_recon true --encoder_rnn_num_layers 4 --encoder_rnn_hidden_size 256 --cov_diag_reg 1e-4 --obj det --use_prior_pi false --use_dim_pi false --vae_alpha 1. --vae_beta 1.1 --vae_gamma 1. --vae_zeta 0.0 --gpuid 0
